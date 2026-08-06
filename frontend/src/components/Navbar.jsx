0d2e0af977697254392c913b1ac37ddfca897fd2
zihad2231 <zihad223193@gmail.com>
2026-08-07 00:40:09 +0600

    Fix navbar bug and add root concurrently setup

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -28,7 +28,7 @@ const Navbar = () => {
           className="navbar-toggler"
           type="button"
           data-bs-toggle="collapse"
-          data-bs-toggle="#navbarNav"
+          data-bs-target="#navbarNav"
           aria-controls="navbarNav"
           aria-expanded="false"
           aria-label="Toggle navigation"
